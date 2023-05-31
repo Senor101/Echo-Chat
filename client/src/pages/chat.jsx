c1102175bcca95926bf25c5e483b56884d676ee0
Senor101 <adikshyanta456@gmail.com>
2023-05-31 08:10:34 +0545

    chat mod

--- client/src/pages/chat.jsx
+++ client/src/pages/chat.jsx
@@ -14,7 +14,6 @@ export default function Chat() {
   const [contacts, setContacts] = useState([]);
   const [currentChat, setCurrentChat] = useState(undefined);
   const [currentUser, setCurrentUser] = useState(undefined);
-  const [userAuth, setUserAuth] = useState(false);
 
   useEffect(() => {
     const getUser = async () => {
